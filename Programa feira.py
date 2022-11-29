5c3a8bf77dfceaacba69fde5e551ffc6ce2ed31a
Leonardo Campos <0000759006@senaimgaluno.com.br>
2022-11-29 19:52:19 -0300

    Adicionado arquivo base do programa, para as equipes desenvolverem.

---
+++ Programa feira.py
@@ -0,0 +1,12 @@
+print ("Olá programador!!")
+print ("O que vamos criar hoje?")
+Nome_programador = "Leonardo Campos"
+print ("Bem vindo" , Nome_programador , ",a partir daqui iniciaremos nossa jornada!")
+print ("Vamos fazer algumas operações usando Python, declarando algumas variáveis")
+preço_banana = 3.5
+preço_maçã = 3.0
+preço_pera = 4.0
+print("Maria foi ao supermecado e comprou doze bananas, quatro maças e cinco peras")
+valor_total = (12*preço_banana) + (4*preço_maçã) + (5*preço_pera)
+print("O valor total, em reais, gasto por Maria foi:" , valor_total)
+
